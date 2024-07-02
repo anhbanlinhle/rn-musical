0db4416c5406fea6dffd28df314edb5e53d60f59
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-02 13:43:50 +0700

    feat[lynn]: create constant & example usage for fonts

--- app/App.jsx
+++ app/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, Text} from 'react-native'
+import Fonts from '../constants/Fonts'
 
 const App = () => {
     return (
         <View style={styles.container}>
-
+            <Text style={{ ...Fonts.regular}}>
+                Poppins Regular
+            </Text>
+            <Text style={{ ...Fonts.bold}}>
+                Poppins Bold
+            </Text>
+            <Text style={{ ...Fonts.italic}}>
+                Poppins Italic
+            </Text>
         </View>
     )
 }
@@ -12,7 +21,9 @@ const App = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 })
 
---
+++ constants/Fonts.js
@@ -0,0 +1,58 @@
+const Fonts = {
+    black: {
+        fontFamily: 'Poppins-Black'
+    },
+    blackItalic: {
+        fontFamily: 'Poppins-BlackItalic'
+    },
+    bold: {
+        fontFamily: 'Poppins-Bold'
+    },
+    boldItalic: {
+        fontFamily: 'Poppins-BoldItalic'
+    },
+    extraBold: {
+        fontFamily: 'Poppins-ExtraBold'
+    },
+    extraBoldItalic: {
+        fontFamily: 'Poppins-ExtraBoldItalic'
+    },
+    extraLight: {
+        fontFamily: 'Poppins-ExtraLight'
+    },
+    extraLightItalic: {
+        fontFamily: 'Poppins-ExtraLightItalic'
+    },
+    italic: {
+        fontFamily: 'Poppins-Italic'
+    },
+    light: {
+        fontFamily: 'Poppins-Light'
+    },
+    lightItalic: {
+        fontFamily: 'Poppins-LightItalic'
+    },
+    medium: {
+        fontFamily: 'Poppins-Medium'
+    },
+    mediumItalic: {
+        fontFamily: 'Poppins-MediumItalic'
+    },
+    regular: {
+        fontFamily: 'Poppins-Regular'
+    },
+    semiBold: {
+        fontFamily: 'Poppins-SemiBold'
+    },
+    semiBoldItalic: {
+        fontFamily: 'Poppins-SemiBoldItalic'
+    },
+    thin: {
+        fontFamily: 'Poppins-Thin'
+    },
+    thinItalic: {
+        fontFamily: 'Poppins-ThinItalic'
+    }
+}
+
+export default Fonts
