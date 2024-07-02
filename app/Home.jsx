36b8f27b515cfdd441da7a379901902c87573c66
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-02 17:14:19 +0700

    feat[lynn]: wip home screen

--- app/Home.jsx
+++ app/Home.jsx
@@ -1,16 +1,74 @@
 import React from 'react'
-import {View, StyleSheet, Button} from 'react-native'
+import {View, StyleSheet, Button, Text, ScrollView, Image, SafeAreaView} from 'react-native'
+
+import {ratioH} from "../utils/converter"
+import {ratioW} from "../utils/converter"
+
+import Fonts from "../constants/Fonts"
+import Icons from "../constants/Icons";
 
 const Home = ({navigation}) => {
+    const Header = () => {
+        return (
+            <View style={styles.homeHeader}>
+                <Image
+                    style={styles.avatar}
+                    source={require('../assets/images/Avatar.png')}
+                />
+                <Text style={styles.greeting}>Hi,{" "}
+                    <Text style={styles.name}>User</Text>
+                </Text>
+                <Icons.Menu/>
+            </View>
+        )
+    }
+
+    const SearchBar = () => {
+        return (
+            <View style={styles.searchBar}>
+                <Text style={styles.searchText}>Search music</Text>
+            </View>
+        )
+    }
+
+    const FeedArea = () => {
+        return (
+            <ScrollView style={styles.feedArea}>
+                {PopularSection()}
+                {TopAlbumSection()}
+            </ScrollView>
+        )
+    }
+
+    const PopularSection = () => {
+        return (
+            <ScrollView
+                horizontal={true}
+                style={styles.popularSection}
+            >
+                <Text style={styles.popularText}>Popular</Text>
+            </ScrollView>
+        )
+    }
+
+    const TopAlbumSection = () => {
+        return (
+            <ScrollView
+                horizontal={true}
+                style={styles.topAlbumSection}
+            >
+                <Text style={styles.topAlbumText}>Top Albums</Text>
+            </ScrollView>
+        )
+    }
+
+
     return (
-        <View style={styles.container}>
-            <Button
-                title="Go to Playlist"
-                onPress={() =>
-                    navigation.navigate('Playlist')
-                }
-            />
-        </View>
+        <SafeAreaView style={styles.container}>
+            {Header()}
+            {SearchBar()}
+            {FeedArea()}
+        </SafeAreaView>
     )
 }
 
@@ -21,6 +79,29 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    homeHeader: {
+        flexDirection: 'row',
+        width: ratioW(327),
+        height: ratioH(41),
+        gap: 4
+    },
+    avatar: {
+        width: ratioW(32),
+        height: ratioH(32),
+    },
+    greeting: {
+        ...Fonts.regular,
+        fontSize: ratioW(32),
+    },
+    name: {
+        ...Fonts.semiBold,
+        fontSize: ratioW(32),
+    },
+    searchText: {
+        ...Fonts.regular,
+    },
+
+
 })
 
 export default Home
