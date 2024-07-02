fc5775052c365346fa4040cfe3b908ef5178bdc4
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-02 11:13:11 +0700

    feat[lynn]: add & clean dependencies

---
+++ app/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+})
+
+export default App
