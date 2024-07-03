bdb859465b3c87b3e65b321d259a62eed7f509f4
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-03 09:09:37 +0700

    feat[lynn]: adjust layout for home

--- app/home/Home.jsx
+++ app/home/Home.jsx
@@ -2,24 +2,36 @@ import React from 'react'
 import {View, StyleSheet, SafeAreaView} from 'react-native'
 import HomeHeader from "./components/HomeHeader"
 import SearchArea from "./components/SearchArea"
-import FeedArea from "./components/FeedArea";
+import FeedArea from "./components/FeedArea"
+import {ratioH, ratioW} from "../../utils/converter"
 
 const Home = ({navigation}) => {
     return (
-        <SafeAreaView style={styles.container}>
-            <HomeHeader/>
-            <SearchArea/>
-            <FeedArea/>
+        <SafeAreaView style={styles.wrapper}>
+            <View style={styles.container}>
+                <HomeHeader/>
+                <SearchArea/>
+                <FeedArea/>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    wrapper: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: '#FFFFFF',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: ratioH(121),
+        padding: [ratioW(0), ratioW(24), ratioW(0), ratioW(24)],
+        gap: ratioW(32)
     }
 })
 
