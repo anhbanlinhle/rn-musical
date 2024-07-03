3cc14039d60a86ec6fcda529126f66c27d2f0e6a
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-04 02:24:03 +0700

    feat[lynn]: footer component for song screen

---
+++ app/song/components/Footer.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+import {ratioH, ratioW} from "../../../utils/converter";
+import Icons from "../../../constants/Icons";
+
+const Footer = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.iconWrapper}>
+                <Icons.HeartFilled style={styles.iconHeart}/>
+            </View>
+            <View style={styles.iconWrapper}>
+                <Icons.Playlist style={styles.iconPlaylist}/>
+            </View>
+            <View style={styles.iconWrapper}>
+                <Icons.Shuffle style={styles.iconShuffle}/>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#A6B9FF',
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
+        width: ratioW(375),
+        height: ratioH(96),
+        position: 'absolute',
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingTop: ratioH(32),
+    },
+    iconWrapper: {
+        width: ratioW(99),
+        height: ratioW(64),
+    },
+    iconHeart: {
+        width: ratioW(32),
+        height: ratioW(32),
+        position: 'absolute',
+        left: 0,
+        marginHorizontal: ratioW(8),
+    },
+    iconPlaylist: {
+        width: ratioW(32),
+        height: ratioW(32),
+        position: 'absolute',
+        right: ratioW(32),
+    },
+    iconShuffle: {
+        width: ratioW(32),
+        height: ratioW(32),
+        position: 'absolute',
+        right: 0,
+        marginHorizontal: ratioW(8),
+    },
+})
+
+export default Footer
