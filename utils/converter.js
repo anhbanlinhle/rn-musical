4b54a3f77b1a8a66e2f3713da623a9b10f934fae
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-04 14:12:15 +0700

    feat[lynn]: add second to time converter for song duration

--- utils/converter.js
+++ utils/converter.js
@@ -11,3 +11,14 @@ export const ratioH = (elementWidth) => {
     // automatically scales to height
     return (elementWidth * device_height) / 812;
 }
+
+export const secondsToTime = (seconds) => {
+    seconds = Math.floor(seconds)
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    const formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`
+}
