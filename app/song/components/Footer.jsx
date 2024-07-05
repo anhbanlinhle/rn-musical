98e516f49469dc3f7abf80a714e6c36c7dcffb18
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-05 15:28:10 +0700

    feat[lynn]: adjust footer height & internal spacing

--- app/song/components/Footer.jsx
+++ app/song/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, TouchableOpacity} from 'react-native'
 import {ratioH, ratioW} from "../../../utils/converter";
 import Icons from "../../../constants/Icons";
 import Images from "../../../constants/Images";
@@ -8,15 +8,15 @@ const Footer = ({color}) => {
 
     return (
         <View style={styles.container(color)}>
-            <View style={styles.iconWrapper}>
+            <TouchableOpacity style={styles.iconWrapper}>
                 <Icons.HeartFilled style={styles.iconHeart}/>
-            </View>
-            <View style={styles.iconWrapper}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconWrapper}>
                 <Icons.Playlist style={styles.iconPlaylist}/>
-            </View>
-            <View style={styles.iconWrapper}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconWrapper}>
                 <Icons.Shuffle style={styles.iconShuffle}/>
-            </View>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 24,
         borderTopRightRadius: 24,
         width: ratioW(375),
-        height: ratioH(96),
+        height: ratioH(96 - 24),
         position: 'absolute',
         bottom: 0,
         alignItems: 'center',
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     iconWrapper: {
         width: ratioW(99),
         height: ratioW(64),
+        // marginVertical: ratioH(16)
     },
     iconHeart: {
         width: ratioW(32),
