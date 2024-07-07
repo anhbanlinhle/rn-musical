c336fa0d8e7a4a8360bcb0ab328173db6834fc1a
anhbanlinhle <vietlinhleviet@gmail.com>
2024-07-08 01:43:18 +0700

    feat[lynn]: add color constant base on theme

---
+++ constants/Colors.js
@@ -0,0 +1,7 @@
+export const backgroundPrimary = (theme) => {
+    return theme === 'light' ? '#F9F6EE' : '#343434'
+}
+
+export const textPrimary = (theme) => {
+    return theme === 'light' ? '#191D21' : '#FAF9F6'
+}
